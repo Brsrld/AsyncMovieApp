276373a2d3dc36c4083551e5d288d05efa96177a
bsrld <brsrld@Brsrld.local>
2023-01-29 14:37:57 +0300

    project was created

---
+++ AsyncMovieApp/AsyncMovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncMovieAppApp.swift
+//  AsyncMovieApp
+//
+//  Created by Barış ŞARALDI on 29.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct AsyncMovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
